85d894ad7e89484638a6c8eb30709db7a1d97827
Annette Bwemere <bwemereannette@gmail.com>
2023-04-06 16:56:26 +0200

    homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './screens/Home';
 export default function App() {
@@ -6,7 +6,7 @@ export default function App() {
     <View style={styles.container}>
       {/* <Text>Open up App.js to start working on your app!</Text> */}
       {/* <StatusBar style="auto" /> */}
-      <Home/>
+      <Home style="auto"/>
     </View>
   );
 }
