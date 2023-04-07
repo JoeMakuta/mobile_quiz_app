0358431c22f3eacd63cf66d06db97cab0f96f39f
Josué Makuta <makutajosue@gmail.com>
2023-04-07 15:07:00 +0200

    Change home page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native'
-import Home from './screens/Home';
-import Question from './screens/Question';
-import Result from './screens/Result';
-import {createNativeStackNavigator} from '@react-navigation/native-stack'
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
+import Question from "./screens/Question";
+import Result from "./screens/Result";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Result'>
-        <Stack.Screen component={Home} name='Home'/>
-        <Stack.Screen component={Question} name='Question'/>
-        <Stack.Screen component={Result} name='Result'/>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen component={Home} name="Home" />
+        <Stack.Screen component={Question} name="Question" />
+        <Stack.Screen component={Result} name="Result" />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -23,10 +23,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    width : '100%',
-    height: '100%'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
   },
 });
