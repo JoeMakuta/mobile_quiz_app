3de728210dd5f331e2e97da6bb9918fa296ceca5
Bonheur Bushiri <bonheurbushiri@gmail.com>
2023-04-12 14:57:21 +0200

    Quizzler

--- src/screens/Question.jsx
+++ src/screens/Question.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-const Question = () => {
+const Question = (navigation) => {
     return (
         <View style={styles.container}>
             <View style={styles.top}>
                 <Text>Quel est le type d'un fichier javascrip?</Text>
             </View>
             <View style={styles.options}>
-                <TouchableOpacity>
+                <TouchableOpacity style={styles.assert}>
+                   <Text>.ts</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.assert}>
+                    <Text>.ts</Text>
+                </TouchableOpacity >
+                <TouchableOpacity style={styles.assert}>
                     <Text>.ts</Text>
                 </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>.ts</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
-                    <Text>.ts</Text>
-                </TouchableOpacity>
-                <TouchableOpacity>
+                <TouchableOpacity style={styles.assert}>
                     <Text>.ts</Text>
                 </TouchableOpacity>
             </View>
             <View style={styles.botton}>
-                <TouchableOpacity><Text>Annulé</Text></TouchableOpacity>
-                <TouchableOpacity><Text>Suivant</Text></TouchableOpacity>
-                <TouchableOpacity><Text>Terminer</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.buttonAnnu}><Text style={styles.buttonText}>ANNULE</Text></TouchableOpacity>
+                <TouchableOpacity style={styles.buttonSuiv}><Text style={styles.buttonText}>SUIVANT</Text></TouchableOpacity>
+                {/* <TouchableOpacity onPress={()=> navigation.navigate("Result")}><Text>TERMINER</Text></TouchableOpacity> */}
             </View>
         </View>
     )
@@ -33,21 +33,53 @@ const Question = () => {
 export default Question
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         padding: 12,
         height: "100%",
+
     },
-    top:{
-        marginVertical:16,
+    top: {
+        marginVertical: 16,
+        marginTop: 40,
     },
-    options:{
-        marginVertical:16,
-        flex:1,
+    options: {
+        marginVertical: 16,
+        marginTop: 100,
+        flex: 1,
+        gap:10,
     },
     botton: {
-        marginBottom:12,
-        paddingVertical:16,
-        justifyContent:"space-between",
-        flexDirection:"row",
+        marginBottom: 12,
+        paddingVertical: 16,
+        justifyContent: "space-between",
+        flexDirection: "row",
+    },
+    buttonSuiv: {
+        backgroundColor: "#314539",
+        padding:12,
+        paddingHorizontal:46,
+        borderRadius: 16,
+        alignItems: "center",
+        marginBottom: 38,
+    },
+    buttonAnnu:{
+        backgroundColor:"#E63030",
+        padding:12,
+        paddingHorizontal:46,
+        borderRadius: 16,
+        alignItems: "center",
+        marginBottom: 38,
+    },
+    buttonText: {
+        fontSize: 18,
+        fontWeight: "bold",
+        color: "white",
+    },
+    assert:{
+        backgroundColor: "#534D4D",
+        height: 50,
+        padding:15,
+        
     }
+
 });
