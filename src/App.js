fc92715b3e73978e537de528ac5c8b8113d972cc
Josué Makuta <makutajosue@gmail.com>
2023-04-13 12:47:12 +0200

    Savings

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
   return (
     <Context.Provider value={{ state, setState }}>
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="Question">
+        <Stack.Navigator initialRouteName="Home">
           <Stack.Screen component={Home} name="Home" />
           <Stack.Screen component={Question} name="Question" />
           <Stack.Screen component={Result} name="Result" />
