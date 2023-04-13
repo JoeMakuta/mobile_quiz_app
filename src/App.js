13e4810be248b5189ab2cb0ed52d31d67bf97364
alexissengangabo07 <alexxngabo@gmail.com>
2023-04-13 17:24:23 +0200

    style review and pass result in navigation params

--- src/App.js
+++ src/App.js
@@ -1,47 +1,32 @@
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import Home from "./screens/Home";
-import Result from "./screens/Result";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useState } from "react";
-import { State } from "./state";
-import { Context } from "./state";
-import Question from "./screens/Quiz";
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import Home from './screens/Home';
+import Quiz from './screens/Quiz';
+import Result from './screens/Result';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-  const [state, setState] = useState(new State());
-
   return (
-    <Context.Provider value={{ state, setState }}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen component={Home} name="Home" />
-          <Stack.Screen
-            component={Question}
-            name="Question"
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            component={Result}
-            options={{ headerShown: false }}
-            name="Result"
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Context.Provider>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen component={Home} name='Home' />
+        <Stack.Screen component={Quiz} name='Quiz' options={{ headerShown: false }} />
+        <Stack.Screen component={Result} name='Result' />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    width: "100%",
-    height: "100%",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%'
   },
 });
--- src/screens/Quiz.jsx
+++ src/screens/Quiz.jsx
@@ -1,129 +1,118 @@
-import { useState } from "react";
-import {
-  StatusBar,
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  Animated,
-  Alert,
-} from "react-native";
-import {
-  Provider as PaperProvider,
-  RadioButton,
-  Button,
-} from "react-native-paper";
-import questions from "../data/questions";
-import ProgressBar from "../components/progressbar/ProgressBar";
+import { useState } from 'react';
+import { StatusBar, View, Text, StyleSheet, TouchableOpacity, Animated, Alert } from 'react-native';
+import { Provider as PaperProvider, RadioButton, Button } from 'react-native-paper';
+import Toast from 'react-native-toast-message';
+import questions from '../data/questions';
+import ProgressBar from '../components/progressbar/ProgressBar';
 
-const Question = ({ navigation }) => {
-  const allQuestions = questions;
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [currentOptionSelected, setCurrentOptionSelected] = useState(null);
-  const [score, setScore] = useState(0);
-  const [progress, setProgress] = useState(new Animated.Value(0));
+const Quiz = ({ navigation }) => {
+    const allQuestions = questions;
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
+    const [currentOptionSelected, setCurrentOptionSelected] = useState(null);
+    const [score, setScore] = useState(0);
+    const [progress, setProgress] = useState(new Animated.Value(0));
 
-  const validateAnswer = () => {
-    let correct_option = allQuestions[currentQuestionIndex]["reponseIndex"];
-    if (currentOptionSelected == correct_option) {
-      // Set Score
-      setScore(score + 1);
+    const validateAnswer = () => {
+        let correct_option = allQuestions[currentQuestionIndex]['reponseIndex'];
+        if (currentOptionSelected == correct_option) {
+            // Set Score
+            setScore(score + 1);
+            Toast.show({
+                type: 'success',
+                text1: 'Succès',
+                text2: 'Bonne réponse, félicitations  👍'
+            });
+        }
+        else {
+            Toast.show({
+                type: 'error',
+                text1: 'Erreur',
+                text2: 'Mauvaise réponse  👎'
+            });
+        }
     }
-  };
 
-  const handleNext = () => {
-    validateAnswer();
-    if (currentQuestionIndex == allQuestions.length - 1) {
-      //   Alert.alert("Votre score est de" + score);
-      navigation.navigate("Result");
-    } else {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-      setCurrentOptionSelected(null);
+    const handleNext = () => {
+        validateAnswer();
+        if (currentQuestionIndex == allQuestions.length - 1) {
+            navigation.navigate('Result', {
+                score,
+                max: allQuestions.length
+            })
+        } else {
+            setCurrentQuestionIndex(currentQuestionIndex + 1);
+            setCurrentOptionSelected(null);
+        }
+        Animated.timing(progress, {
+            toValue: currentQuestionIndex + 1,
+            duration: 1000,
+            useNativeDriver: false
+        }).start();
     }
-    Animated.timing(progress, {
-      toValue: currentQuestionIndex + 1,
-      duration: 1000,
-      useNativeDriver: false,
-    }).start();
-  };
 
-  return (
-    <PaperProvider>
-      <View style={styles.screenContainer}>
-        <View>
-          <View>
-            <ProgressBar allQuestions={allQuestions} progress={progress} />
-            <Text style={styles.questionCounter}>
-              {currentQuestionIndex + 1} / {allQuestions.length}
-            </Text>
-            <Text style={styles.questionLabel}>
-              {allQuestions[currentQuestionIndex]?.titre}
-            </Text>
-            <Text>Score: {score}</Text>
-          </View>
-          <View>
-            <RadioButton.Group
-              onValueChange={(value) => setCurrentOptionSelected(value)}
-              value={currentOptionSelected}
-            >
-              {allQuestions[currentQuestionIndex].assertions.map(
-                (assertion, index) => (
-                  <RadioButton.Item
-                    key={index}
-                    label={assertion}
-                    value={index}
-                    uncheckedColor="#000"
-                    color="green"
-                    style={styles.radioItems}
-                  />
-                )
-              )}
-            </RadioButton.Group>
-          </View>
-        </View>
-        <View>
-          <Button
-            mode="contained"
-            onPress={handleNext}
-            buttonColor="green"
-            style={styles.buttonNext}
-            disabled={currentOptionSelected === null ? true : false}
-          >
-            SUIVANT
-          </Button>
-        </View>
-      </View>
-    </PaperProvider>
-  );
-};
+    return (
+        <PaperProvider>
+            <Toast
+                position='bottom'
+                visibilityTime={1000}
+            />
+            <View style={styles.screenContainer}>
+                <View>
+                    <View>
+                        <ProgressBar allQuestions={allQuestions} progress={progress} />
+                        <Text style={styles.questionCounter}>{currentQuestionIndex + 1} / {allQuestions.length}</Text>
+                        <Text style={styles.questionLabel}>
+                            {allQuestions[currentQuestionIndex]?.titre}
+                        </Text>
+                    </View>
+                    <View>
+                        <RadioButton.Group onValueChange={value => setCurrentOptionSelected(value)} value={currentOptionSelected}>
+                            {
+                                allQuestions[currentQuestionIndex].assertions.map((assertion, index) => (
+                                    <RadioButton.Item key={index} label={assertion} value={index} uncheckedColor='#000' color='green' style={styles.radioItems} />
+                                ))
+                            }
+                        </RadioButton.Group>
+                    </View>
+                </View>
+                <View>
+                    <Button mode="contained" onPress={handleNext} buttonColor='green' style={styles.buttonNext} disabled={currentOptionSelected === null ? true : false}>
+                        SUIVANT
+                    </Button>
+                </View>
+            </View>
+        </PaperProvider>
+    )
+}
 
 const styles = StyleSheet.create({
-  screenContainer: {
-    flex: 1,
-    justifyContent: "space-between",
-    paddingBottom: 15,
-    marginTop: 50,
-  },
-  radioItems: {
-    backgroundColor: "lightgray",
-    borderRadius: 6,
-    paddingVertical: 15,
-    marginVertical: 6,
-  },
-  questionCounter: {
-    color: "green",
-    fontSize: 15,
-    fontWeight: "bold",
-  },
-  questionLabel: {
-    // fontWeight: 'bold',
-    fontSize: 20,
-    marginVertical: 15,
-  },
-  buttonNext: {
-    borderRadius: 6,
-    paddingVertical: 10,
-  },
+    screenContainer: {
+        flex: 1,
+        justifyContent: 'space-between',
+        paddingBottom: 15,
+        paddingHorizontal: 6,
+        marginTop: 50
+    },
+    radioItems: {
+        backgroundColor: 'lightgray',
+        borderRadius: 6,
+        paddingVertical: 15,
+        marginVertical: 6
+    },
+    questionCounter: {
+        color: 'green',
+        fontSize: 15,
+        fontWeight: 'bold'
+    },
+    questionLabel: {
+        // fontWeight: 'bold',
+        fontSize: 20,
+        marginVertical: 15
+    },
+    buttonNext: {
+        borderRadius: 6,
+        paddingVertical: 6
+    }
 });
 
-export default Question;
+export default Quiz;
